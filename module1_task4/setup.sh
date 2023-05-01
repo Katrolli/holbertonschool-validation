7f0cd87e89129d4ee31b610707aca69560f6bbff
Katrolli <ennio_cano@live.com>
2023-05-01 18:05:48 +0200

    idk what this is

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,4 +1,3 @@
-apt update
-apt install hugo
-apt install make
+#!/bin/bash
+apt-get update && apt-get install -y git make wget build-essential && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb 
 make build
